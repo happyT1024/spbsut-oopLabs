466aa21f2f763439e020958cc98650d48d790814
Beliar <68490921+happyT1024@users.noreply.github.com>
2022-09-10 17:47:15 +0300

    Create CTwo.h

---
+++ lab2/CTwo.h
@@ -0,0 +1,45 @@
+#include "COne.h"
+#include <iostream>
+#ifndef HELLOWORLD2_CTWO_H
+#define HELLOWORLD2_CTWO_H
+using std::cout;
+
+class CTwo{
+public:
+    CTwo()
+    :d(0.0)
+    ,p(new COne)
+    {}
+
+    CTwo(COne const * _p,double d)
+    :d(d)
+    ,p(new COne)
+    {
+        *this->p=*_p;
+    }
+
+    CTwo(CTwo const & cTwo)
+    :d(cTwo.d)
+    {
+        *this->p=*cTwo.p;
+    }
+
+    CTwo & operator=(CTwo const & cTwo);
+
+    double getD() const;
+
+    void setD(double d);
+
+    COne *getP() const;
+
+    void setP(COne *p);
+
+    void print() const;
+
+    ~CTwo(){
+        delete p;
+    }
+private:
+    COne* p;
+    double d;
+};
